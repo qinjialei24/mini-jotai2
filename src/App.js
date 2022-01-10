dc6500a9aa2494c7287a7f5bf9782afa05417643
jialei qin <jialei.qin@thoughtworks.com>
2022-01-10 21:25:55 +0800

    简单实现全局state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
+const listeners = new Set();
+
+let globalState ={
+  count:0
+}
+
+const useGlobalState = () => {
+  const [state,setState] = useState(globalState);
+
+  useEffect(() => {
+    const listener = () => {
+      setState(globalState);
+    };
+    listeners.add(listener);
+    listener(); // in case it's already changed
+    return () => listeners.delete(listener); // cleanup
+  }, []);
+
+  const setGlobalState = (nextGlobalState) => {
+    globalState = nextGlobalState
+    listeners.forEach(listener => listener());
+  }
+
+  return [state,setGlobalState]
+};
+
+
+function Comp1() {
+  const [xxx1] = useGlobalState()
+  return <h1>Comp1: {xxx1.count}</h1>
+}
+
+function Comp2() {
+  const [xxx2] = useGlobalState()
+  return <h1>Comp1: {xxx2.count}</h1>
+}
+
 
 function App() {
+  const [,update] = useGlobalState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comp1></Comp1>
+      <Comp2></Comp2>
+      <button onClick={() =>{
+        update({
+          ...globalState,
+          count:globalState.count+1
+        })
+      }}>increase</button>
     </div>
   );
 }
